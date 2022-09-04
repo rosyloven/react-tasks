d1e6cb27d47262b943cbe46d0739f849d26b19ea
Богдан Чайка <bogdancajka@MacBook-Pro-Bogdan.local>
2022-09-04 15:01:34 +0300

    Changes

--- src/UI/components/content/TabBar/Table/index.js
+++ src/UI/components/content/TabBar/Table/index.js
@@ -6,15 +6,13 @@ import {
 } from './views'
 
 const Table = ({ data, titles }) => {
-  const preparedData = data.map((person) => Object.values(person))
-
   return (
     <StyledTableContainer dataSize={data.length} titleSize={titles.length}>
       {titles.map((title, index) => (
         <StyledTableHeadCell key={index}>{title}</StyledTableHeadCell>
       ))}
-      {preparedData.map((el) =>
-        el.map((person, index) => (
+      {data.map((el) =>
+        Object.values(el).map((person, index) => (
           <StyledTableContentCell key={index}>{person}</StyledTableContentCell>
         ))
       )}
